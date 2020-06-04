3ace01dede877990f4ed23a4b927f0089a027072
diogosalesdev <diogosalesdev@gmail.com>
2020-06-04 20:56:32 -0300

    Using Template Engine with nunjucks

--- src/server.js
+++ src/server.js
@@ -4,17 +4,28 @@ const server = express()
 //Configurando pasta pública
 server.use(express.static("public"))
 
+// Utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true
+})
+
 // Configurar caminhos para a aplicação
 
 // página inicial
 // req: Requisição
 // res: Resposta
 server.get("/", (req,res) => {
-  res.sendFile(__dirname + "/views/index.html")
+  return res.render("index.html")
 })
 
 server.get("/create-point", (req,res) => {
-  res.sendFile(__dirname + "/views/create-point.html")
+  return res.render("create-point.html")
+})
+
+server.get("/search", (req,res) => {
+  return res.render("search-results.html")
 })
 
 
