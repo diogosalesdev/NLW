12816cfa1e01eb94c64b311a8ee7485807a0835d
diogosalesdev <diogosalesdev@gmail.com>
2020-06-04 16:53:39 -0300

    Inserting routes in file server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,23 @@
 const express = require("express")
 const server = express()
 
+//Configurando pasta pública
+server.use(express.static("public"))
+
+// Configurar caminhos para a aplicação
+
+// página inicial
+// req: Requisição
+// res: Resposta
+server.get("/", (req,res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req,res) => {
+  res.sendFile(__dirname + "/views/create-point.html")
+})
+
+
 // Ligar o servidor
 
 server.listen(3000)
