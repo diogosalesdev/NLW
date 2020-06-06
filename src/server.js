00efc0462092e48ee95f78b5701badf1f1fd8707
diogosalesdev <diogosalesdev@gmail.com>
2020-06-05 22:13:15 -0300

    Site finished and ready for improvements

--- src/server.js
+++ src/server.js
@@ -66,8 +66,8 @@ server.post("/savepoint", (req, res) => {
         console.log(err)
         return res.send("Erro no cadastro!")
       }
-      console.log("Cadastrado com sucesso")
-      console.log(this)
+      //console.log("Cadastrado com sucesso")
+      //console.log(this)
 
       return res.render("create-point.html", {saved: true})
     }
@@ -76,14 +76,25 @@ server.post("/savepoint", (req, res) => {
 })
 
 server.get("/search", (req,res) => {
+  
+  const search = req.query.search
+
+  if(search == "") {
+    //pesquisa vazia
+    return res.render("search-results.html", {total: 0})
+  }
+
   //Pegar os dados do banco de dados
 
-  db.all(`SELECT * FROM places`, function(err, rows) {
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+    
     if(err) {
       return console.log(err)
     }
+    
     const total = rows.length
     return res.render("search-results.html", {places: rows, total})
+    
   })
 
   
