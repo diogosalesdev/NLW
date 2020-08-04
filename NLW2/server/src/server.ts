e871b01af1e7d21abc85e2d06ee5e1c47304e638
diogosalesdev <diogosalesdev@gmail.com>
2020-08-04 13:49:03 -0300

    Database connection created with knex and sqlite

--- NLW2/server/src/server.ts
+++ NLW2/server/src/server.ts
@@ -2,13 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  const users = [
-    {name: 'Diogo', age: 38},
-    {name: 'Thiago', age: 32},
-    {name: 'Victor', age: 27},
-  ]
-  return res.json(users);
+app.use(express.json());
+
+app.get('/', (req, res) => {
+   return res.json({message: "Hello World!"});
 });
 
 app.listen(3333);
