d112fec38b0dbc0ccbfdcfef55067d5932c8d3bc
diogosalesdev <diogosalesdev@gmail.com>
2020-08-05 20:24:35 -0300

    Components Sect and Textarea created

--- NLW2/web/src/pages/TeacherForm/index.tsx
+++ NLW2/web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader/index';
 import Input from '../../components/Input/index';
+import Textarea from '../../components/Textarea/index';
+import Select from '../../components/Select/index';
 
 import warningIcon from '../../assets/images/icons/warning.svg'
 
@@ -18,11 +20,29 @@ function TeacherForm() {
           <legend>Seus dados</legend>
           <Input name="name" label="Nome completo" />
           <Input name="avatar" label="Avatar" />
-          <Input name="whatsapp" label="Whatsapp" />          
+          <Input name="whatsapp" label="Whatsapp" /> 
+          <Textarea name="bio" label="Biografia" />         
         </fieldset>
          <fieldset>
           <legend>Sobre a aula</legend>
-          <Input name="subject" label="Matéria" />
+          <Select 
+            name="subject"
+            label="Matéria"
+            options={[
+              {value: 'Artes', label: 'Artes'},
+              {value: 'Biologia', label: 'Biologia'},
+              {value: 'Física', label: 'Física'},
+              {value: 'Matemática', label: 'Matemática'},
+              {value: 'Português', label: 'Português'},
+              {value: 'História', label: 'História'},
+              {value: 'Geografia', label: 'Geografia'},
+              {value: 'Literatura', label: 'Literatura'},
+              {value: 'Literatura', label: 'Literatura'},
+              {value: 'Inglês', label: 'Inglês'},
+              {value: 'Educação física', label: 'Educação física'},
+              {value: 'Química', label: 'Química'},
+            ]}
+          />
           <Input name="cost" label="Custo da sua hora por aula" />                 
         </fieldset>
         <footer>
