e97324468afc1de4171639323fb2e1b1a42990d8
diogosalesdev <diogosalesdev@gmail.com>
2020-08-05 11:40:49 -0300

    Class criation route created

--- NLW2/server/src/routes.ts
+++ NLW2/server/src/routes.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import db from './database/connection';
 
+import convertHourToMinutes from './utils/convertHoursToMinutes';
+
 const routes = express.Router();
+
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string;
+}
 
 routes.post('/classes', async (req, res) => {
   const {
@@ -13,15 +21,48 @@ routes.post('/classes', async (req, res) => {
     cost,
     schedule
   } = req.body;
+
+  const trx = await db.transaction();
   
-  await db('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    bio
-  });
-  
-  return res.send();
+  try {
+    const insertedUsersIds = await trx('users').insert({
+      name,
+      avatar,
+      whatsapp,
+      bio
+    });
+
+    const user_id = insertedUsersIds[0];
+
+    const insertedClassesIds = await trx('classes').insert({
+      subject,
+      cost,
+      user_id
+    });
+
+    const class_id = insertedClassesIds[0];
+
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+      return {
+        class_id,
+        week_day: scheduleItem.week_day,
+        from: convertHourToMinutes(scheduleItem.from),
+        to: convertHourToMinutes(scheduleItem.to)
+      };
+    });
+
+    await trx('class_schedule').insert(classSchedule);
+
+    await trx.commit();
+
+    return res.status(201).send();
+    
+  } catch (err) {
+    await trx.rollback();
+    return res.status(400).json({
+      error: 'Unexpected error while creating new class'
+    });
+  }
 });
 
 export default routes;
