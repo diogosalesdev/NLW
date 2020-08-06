bfe445962ebcdea69d4294d56443cda834c77dd5
diogosalesdev <diogosalesdev@gmail.com>
2020-08-06 17:46:28 -0300

    All Proffy page front-end finished and connected with database through a api

--- NLW2/web/src/components/TeacherItem/index.tsx
+++ NLW2/web/src/components/TeacherItem/index.tsx
@@ -1,33 +1,50 @@
 import React from 'react';
+import api from '../../services/api';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {  
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/65976669?s=460&u=f010266914f8585f92d3c678fd2a82724a59b57a&v=4" alt="Diogo Sales"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Diogo Sales</strong>
-          <span>Física</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Texto
-        <br/><br/>
-        hafjkndjkvndmsvdf ksfbnkçasdnmfsdm,.nf.sd,mf jshfjskdhnçfajsdf kjshdfkjsdhnfjksnldf
-      </p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} >
           <img src={whatsappIcon} alt="whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
--- NLW2/web/src/pages/TeacherList/index.tsx
+++ NLW2/web/src/pages/TeacherList/index.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import api from '../../services/api';
+
 import PageHeader from '../../components/PageHeader/index';
-import TeacherItem from '../../components/TeacherItem/index';
+import TeacherItem, {Teacher} from '../../components/TeacherItem/index';
 import Input from '../../components/Input/index';
 import Select from '../../components/Select/index';
 
 import './styles.css';
 
 function TeacherList() {
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekday] = useState('');
+  const [time, setTime] = useState('');
+
+async function searchTeachers(e: FormEvent) {
+  e.preventDefault();
+  const res = await api.get('classes', {
+    params: {
+      subject,
+      week_day,
+      time
+    }
+  });
+  setTeachers(res.data);
+}
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os professores disponíveis.">
-        <form id="search-teachers">
+        <form id="search-teachers" onSubmit={searchTeachers} >
            <Select 
             name="subject"
             label="Matéria"
+            value={subject}
+            onChange={(e) => {setSubject(e.target.value)}}
             options={[
               {value: 'Artes', label: 'Artes'},
               {value: 'Biologia', label: 'Biologia'},
@@ -32,6 +54,8 @@ function TeacherList() {
            <Select 
             name="week_day"
             label="Dia da semana"
+            value={week_day}
+            onChange={(e) => {setWeekday(e.target.value)}}
             options={[
               {value: '0', label: 'Domingo'},
               {value: '1', label: 'Segunda-feira'},
@@ -42,15 +66,17 @@ function TeacherList() {
               {value: '6', label: 'Sábado'}
             ]}
           />
-          <Input type="time" name="time" label="Hora" />          
+          <Input type="time" name="time" label="Hora" value={time}
+            onChange={(e) => {setTime(e.target.value) }}/>
+          <button type="submit">
+            Buscar
+          </button>         
         </form>
       </PageHeader>
       <main>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} />;
+        })}
       </main>
     </div>
   );
