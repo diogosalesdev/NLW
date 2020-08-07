7274ac89c96da69f005125d3b4ad245fae15ae4b
diogosalesdev <diogosalesdev@gmail.com>
2020-08-07 16:20:48 -0300

    Routes file created and Landing page finished

--- NLW2/mobile/App.tsx
+++ NLW2/mobile/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Landing from './src/pages/Landing/index';
+import AppStack from './src/routes/AppStack';
 import { AppLoading } from 'expo';
 
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
@@ -18,7 +18,7 @@ export default function App() {
   } else {
     return (
       <> 
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />  
       </>
     );
