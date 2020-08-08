8ba81749280c0b8f82b9dfe501b7ec56757507d3
diogosalesdev <diogosalesdev@gmail.com>
2020-08-07 21:32:32 -0300

    Page header component finished

--- NLW2/mobile/src/pages/Favorites/index.tsx
+++ NLW2/mobile/src/pages/Favorites/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader/index';
+
 import styles from './styles.ts'
 
 function Favorites() {
-  return <View style={styles.container} />
+  return (
+    <View style={styles.container}>
+     <PageHeaderr title="Meus proffys favoritos" />
+    </View>
+  );
 }
 
 export default Favorites;
