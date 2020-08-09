eda98ddabb8bc5f6e8adcd588b31d1e69d82fb3d
diogosalesdev <diogosalesdev@gmail.com>
2020-08-08 21:56:15 -0300

    Whatsapp linking finished

--- NLW2/mobile/src/components/TeacherItem/index.tsx
+++ NLW2/mobile/src/components/TeacherItem/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-hundler';
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
@@ -8,35 +8,49 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
 import './styles.ts'
 
-function TeacherItem() {
+export interface Teacher {  
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+  function handleLinkToWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
+  }
+
   return (
     <View style={styles.container} >
       <View style={styles.profile}>
         <Image 
           style={syles.avatar}
-          source={{uri: 'http://github.com/diogosalesdev.png'}}
+          source={{uri: teacher.avatar}}
         />
         <View style={styles.profileInfo} >
-          <Text style={styles.name} >Dr. Jekyll</Text>
-          <Text style={styles.subject} >Física</Text>
+          <Text style={styles.name} >{teacher.name}</Text>
+          <Text style={styles.subject} >{teacher.subject}</Text>
         </View>
       </View>
-      <Text style={styles.bio} >
-        fhjsdhkfjksdfjdnf jhskdhjfgkdjsfg jhfdhafgçjdfg ljhjfkg jhkjfhnsd jndf
-        {'\n'}{'\n'}
-        jkflvgdfç knj mjnh mk lknvçklvnaç çlvknsdçvkç mlkmçmkmçkmçkmç kmkmçkmç
-      </Text>
+      <Text style={styles.bio} >{teacher.bio} </Text>
       <View style={syles.footer} >
         <Text style={styles.price} >
           Preço/Hora {'   '}
-          <Text style={styles.priceValue} >R$ 80,00</Text>
+          <Text style={styles.priceValue} >{teacher.cost}</Text>
         </Text>
         <View style={styles.buttonsContainer} >
           <RectButton style={[styles.favoriteButton, styles.favorited]}>
             {/* <Image source={heartOutlineIcon} /> */}
             <Image source={unFavoriteIcon} />
           </RectButton>
-          <RectButton style={styles.contactButton}>
+          <RectButton onPress={handleLinkToWhatsapp} style={styles.contactButton}>
             <Image source={whatsappIcon} />
             <Text style={styles.contactButtonText} >
               Entrar em contato
--- NLW2/mobile/src/pages/TeacherList/index.tsx
+++ NLW2/mobile/src/pages/TeacherList/index.tsx
@@ -4,13 +4,32 @@ import { BorderlessButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 
 import PageHeader from '../../components/PageHeader/index';
-import TeacherItem from '../../components/TeacherItem/index';
+import TeacherItem, { Teacher } from '../../components/TeacherItem/index';
 
 import styles from './styles.ts';
 import { RectButton } from 'react-native-gesture-hundler';
+import api from '../../services/api';
 
 function TeacherList() {
   const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  const [teachers, setTeachers] = useState([]);
+
+  async function handleFiltersSubmit() {
+    const res = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    });
+    setIsFiltersVisible(false);
+    setTeachers(res.data);
+  }
 
   function handleToggleFilterVisible() {
     setIsFiltersVisible(!isFiltersVisible);
@@ -31,6 +50,8 @@ function TeacherList() {
           <Text style={styles.label}>Matéria</Text>
           <TextInput
             style={styles.input}
+            value={subject}
+            onChangeText={text => setSubject(text)}
             placeholder="Qual a matéria?"
             placeholderTextColor="#c1bccc"
           />
@@ -39,6 +60,8 @@ function TeacherList() {
               <Text style={styles.label}>Dia da semana</Text>
               <TextInput
                 style={styles.input}
+                value={week_day}
+                onChangeText={text => setWeekDay(text)}
                 placeholder="Qual o dia?"
                 placeholderTextColor="#c1bccc"
               />
@@ -47,19 +70,22 @@ function TeacherList() {
               <Text style={styles.label}>Horário</Text>
               <TextInput
                 style={styles.input}
+                value={time}
+                onChangeText={text => setTime(text)}
                 placeholder="Qual o horário?"
                 placeholderTextColor="#c1bccc"
               />
             </View>
           </View>
-          <RectButton style={styles.submitButton} >
+          <RectButton onPress={handleFiltersSubmit} style={styles.submitButton} >
             <Text style={styles.submitButtonText} >Filtrar</Text>
           </RectButton>
         </View>
       )}
       </PageHeader>
       <ScrollView style={styles.teacherList} contentContainerStyle={{paddingHorizontal: 16, paddingBottom: 16}} >
-       <TeacherItem />
+       {teachers.map(teacher: Teacher => <TeacherItem key={teacher.id} teacher={teacher} />)}
+       
        <TeacherItem />
        <TeacherItem />
        <TeacherItem />
